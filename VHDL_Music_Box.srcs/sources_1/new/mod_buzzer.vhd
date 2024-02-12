8b017573f154e1ea312852858244c0ec6b768966
christian-dale <jchaukvik@gmail.com>
2024-02-12 10:56:48 +0100

    Add second itertor of midi square wave generator (buzzer)

--- VHDL_Music_Box.srcs/sources_1/new/mod_buzzer.vhd
+++ VHDL_Music_Box.srcs/sources_1/new/mod_buzzer.vhd
@@ -15,29 +15,43 @@ entity mod_buzzer is
     );
 
     port (
-        clk: in std_logic;
-        JA1: inout std_logic
+        signal clk: in std_logic;
+        signal JA1: inout std_logic;
+        signal sw_0: in std_logic
     );
 end mod_buzzer;
 
 architecture Behavioral of mod_buzzer is
-    -- Formula (midi note to frequeny): f = 2^((n - 69) / 12) * 440
+    -- Formula (midi note to frequency): f = 2^((n - 69) / 12) * 440
     function midi_to_hz(note_number: integer) return integer is
         constant a4_hz: integer := 440;
     begin
         return 2 ** ((note_number - 69) / 12) * a4_hz;
     end function midi_to_hz;
+    
+    function hz_to_clk(hz: integer) return integer is
+    begin
+        return (CLK_HZ / hz) / 2;
+    end function hz_to_clk;
 begin
     process (clk)
         variable i: integer := 0;
+        variable note: integer := 90;
+        variable toggle: boolean := false;
     begin
         if rising_edge(clk) then
             -- Halve the number here to get half a period.
-            if i = (CLK_HZ / midi_to_hz(69)) / 2 then
+            if i = hz_to_clk(midi_to_hz(note)) then
                 i := 0;
                 JA1 <= not JA1;
             else
                 i := i + 1;
+            end if;
+            
+            -- Check switch
+            if sw_0 = '1' and toggle = false then
+                toggle := true;
+                note := 69;
             end if;
         end if;
     end process;
