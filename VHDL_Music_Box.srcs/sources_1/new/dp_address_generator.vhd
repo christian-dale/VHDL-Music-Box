4a55b1d4c1c62c4e0014ef9d22011acd4d6cdeb8
VendelK <panleo97@hotmail.com>
2024-02-12 15:07:09 +0100

    Finished address generator for the RAM

--- VHDL_Music_Box.srcs/sources_1/new/dp_address_generator.vhd
+++ VHDL_Music_Box.srcs/sources_1/new/dp_address_generator.vhd
@@ -21,25 +21,29 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity dp_address_generator is
-    Port ( inc : in STD_LOGIC;
-           rst : in STD_LOGIC;
+    Port ( inc : in STD_LOGIC; -- increment
+           rst : in STD_LOGIC; -- reset
+		   clk : in STD_LOGIC; -- clock
            address_out : out STD_LOGIC_VECTOR (9 downto 0));
 end dp_address_generator;
 
 architecture Behavioral of dp_address_generator is
-
+	signal counter : INTEGER range 0 to 1023 := 0;
 begin
-
-
+	process(clk, rst)
+	begin
+		if rst = '1' then
+			counter <= 0;
+		elsif rising_edge(clk) then
+			if inc = '1' then
+				counter <= counter+1;
+			end if;
+		end if;
+	end process;
+	
+	address_out <= STD_LOGIC_VECTOR(to_unsigned(counter, address_out'length));
+	
 end Behavioral;
